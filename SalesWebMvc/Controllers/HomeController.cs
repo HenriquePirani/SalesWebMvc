f07db1d79be6452661d6f697ce7d075919d1ee8f
HenriquePirani <henriquepirani@gmail.com>
2022-05-12 14:53:51 +0100

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+            ViewData["Message2"] = "Not of your business";
 
             return View();
         }
